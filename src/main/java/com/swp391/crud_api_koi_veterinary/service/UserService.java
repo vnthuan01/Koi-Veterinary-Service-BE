cb36f6a7f4aab3ac34ada3cec81a8415e60642b2
KhangHuynh <khanghqse182958@fpt.edu.vn>
2024-09-26 12:16:47 +0700

    Mã hóa mật khẩu
    Mã hóa mật khẩu khi tạo 1 account

--- src/main/java/com/swp391/crud_api_koi_veterinary/service/UserService.java
+++ src/main/java/com/swp391/crud_api_koi_veterinary/service/UserService.java
@@ -5,6 +5,8 @@ import com.swp391.crud_api_koi_veterinary.model.entity.UserAccount;
 import com.swp391.crud_api_koi_veterinary.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,10 +19,13 @@ public class UserService {
     public UserAccount createUser(UserCreationRequest request) {
         UserAccount user = new UserAccount();
         user.setUsername(request.getUsername());
-        user.setPassword(request.getPassword());
+        //mã hóa password dưới BD theo BCrypt
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
         user.setEmail(request.getEmail());
         user.setFirstName(request.getFirstName());
         user.setLastName(request.getLastName());
+        user.setRole(request.getRole());
         user.setAddress(request.getAddress());
         user.setPhone(request.getPhone());
         return userRepository.save(user);
